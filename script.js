69d0fbd3785fbe2b6ab5d88d5f22a246699c5cfe
ARSN <arsnishere@gmail.com>
2024-03-14 13:48:22 +0530

    FinalPush

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
+function toggleImage() {
+    const SlotFull = document.getElementById('SlotFull');
+    const SlotEmpty = document.getElementById('SlotEmpty');
 
-const slot1 = document.getElementById('slot1');
-const slot2 = document.getElementById('slot2');
-
-function parkCar(slot) {
-  slot.style.backgroundColor = 'blue';
+    if (image1.style.display !== 'none') {
+        image1.style.display = 'none';
+        image2.style.display = 'block';
+    } else {
+        image1.style.display = 'block';
+        image2.style.display = 'none';
+    }
 }
-
-function leaveParking(slot) {
-  slot.style.backgroundColor = 'red';
-}
-
-parkCar(slot1);
-
-setTimeout(() => leaveParking(slot2), 3000); // Change to setTimeout(leaveParking, 3000, slot2); for compatibility with older browsers
